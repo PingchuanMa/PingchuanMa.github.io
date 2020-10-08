5ada5e84133e3d590f981034aab94f579d6f0c2f
Pingchuan Ma <pika7ma@gmail.com>
2020-10-08 17:14:39 -0400

    simplify js

--- js/script.js
+++ js/script.js
@@ -1,12 +1,3 @@
-$(function () {
-  $('[data-toggle="popover"]').popover({
-    html: true,
-    content: function () {
-      return '<img class="img-fluid" width="100px" src="'+$(this).data('img') + '" />';
-    }
-  })
-})
-
 $(document).ready(function() {
   $('#eduContent').on('hide.bs.collapse', function () {
     var trigger = document.getElementById('eduTrigger');
