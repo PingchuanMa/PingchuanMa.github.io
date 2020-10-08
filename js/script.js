0a7e4275dfc710ed07f9a2368837950edc59b092
Pingchuan Ma <pika7ma@gmail.com>
2020-10-08 18:31:47 -0400

    add title for implicit href

--- js/script.js
+++ js/script.js
@@ -2,20 +2,24 @@ $(document).ready(function() {
   $('#eduContent').on('hide.bs.collapse', function () {
     var trigger = document.getElementById('eduTrigger');
     trigger.innerText = '[show]';
+    trigger.title = 'Show education';
   })
 
   $('#eduContent').on('show.bs.collapse', function () {
     var trigger = document.getElementById('eduTrigger');
     trigger.innerText = '[hide]';
+    trigger.title = 'Hide education';
   })
 
   $('#expContent').on('hide.bs.collapse', function () {
     var trigger = document.getElementById('expTrigger');
     trigger.innerText = '[show]';
+    trigger.title = 'Show experience';
   })
 
   $('#expContent').on('show.bs.collapse', function () {
     var trigger = document.getElementById('expTrigger');
     trigger.innerText = '[hide]';
+    trigger.title = 'Hide experience';
   })
 })
