ed9a69bccf2bc3d4623f428451b378e312d1f7bb
Pingchuan Ma <pika7ma@gmail.com>
2021-01-31 00:53:14 -0500

    upgrade bootstrap

--- js/script.js
+++ js/script.js
@@ -1,25 +1,5 @@
-$(document).ready(function() {
-  $('#eduContent').on('hide.bs.collapse', function () {
-    var trigger = document.getElementById('eduTrigger');
-    trigger.innerText = '[show]';
-    trigger.title = 'Show education';
-  })
-
-  $('#eduContent').on('show.bs.collapse', function () {
-    var trigger = document.getElementById('eduTrigger');
-    trigger.innerText = '[hide]';
-    trigger.title = 'Hide education';
-  })
-
-  $('#expContent').on('hide.bs.collapse', function () {
-    var trigger = document.getElementById('expTrigger');
-    trigger.innerText = '[show]';
-    trigger.title = 'Show experience';
-  })
-
-  $('#expContent').on('show.bs.collapse', function () {
-    var trigger = document.getElementById('expTrigger');
-    trigger.innerText = '[hide]';
-    trigger.title = 'Hide experience';
-  })
+var myCollapsible = document.getElementById('moreContent')
+myCollapsible.addEventListener('show.bs.collapse', function () {
+  var trigger = document.getElementById('moreTrigger');
+  trigger.style.visibility = "hidden";
 })
