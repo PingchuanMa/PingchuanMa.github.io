2a886cf33e20716ad64b693cd75cbb8c24ce0442
Pingchuan Ma <pika7ma@gmail.com>
2021-03-18 17:42:05 -0400

    add cat script

--- js/script.js
+++ js/script.js
@@ -1,6 +1,22 @@
-var myCollapsible = document.getElementById('moreContent')
-myCollapsible.addEventListener('shown.bs.collapse', function () {
-  var trigger = document.getElementById('moreTrigger');
+var moreContent = document.getElementById('moreContent');
+moreContent.addEventListener('shown.bs.collapse', function () {
+  let trigger = document.getElementById('moreTrigger');
   trigger.style.visibility = "hidden";
   this.scrollIntoView();
+});
+
+var counter = 0;
+var hoverImages = [
+  'images/McFlurry.png',
+  'images/Sophie.png',
+  'images/Sofia.png',
+];
+
+var profile = document.getElementById('profile');
+profile.addEventListener('mouseenter', function() {
+  this.src = hoverImages[counter];
+  counter = (counter + 1) % hoverImages.length;
+})
+profile.addEventListener('mouseleave', function() {
+  this.src = 'images/myself.png';
 })
