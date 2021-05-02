321db63c4a658c3bd9ccbbfc5e282296e61675a0
Pingchuan Ma <pika7ma@gmail.com>
2021-05-02 14:45:47 -0400

    format

--- js/script.js
+++ js/script.js
@@ -1,23 +1,26 @@
-var moreContent = document.getElementById('moreContent');
-moreContent.addEventListener('shown.bs.collapse', function () {
-  let trigger = document.getElementById('moreTrigger');
+var moreContent = document.getElementById("moreContent");
+moreContent.addEventListener("shown.bs.collapse", function() {
+  let trigger = document.getElementById("moreTrigger");
   trigger.style.visibility = "hidden";
   this.scrollIntoView();
 });
 
 var counter = 0;
 var hoverImages = [
-  'images/profile/McFlurry.png',
-  'images/profile/Sophie.png',
-  'images/profile/Sofia.png',
-  'images/profile/Mayo.png',
+  "images/profile/McFlurry.png",
+  "images/profile/Sophie.png",
+  "images/profile/Sofia.png",
+  "images/profile/Mayo.png",
 ];
 
-var profile = document.getElementById('profile');
-profile.addEventListener('mouseenter', function() {
+var profile = document.getElementById("profile");
+profile.addEventListener("mouseenter", function() {
   this.src = hoverImages[counter];
   counter = (counter + 1) % hoverImages.length;
-})
-profile.addEventListener('mouseleave', function() {
-  this.src = 'images/profile/myself.png';
-})
+});
+profile.addEventListener("mouseleave", function() {
+  this.src = "images/profile/myself.png";
+});
+
+var d = new Date();
+document.getElementById('year').innerHTML = d.getFullYear();
