59f804ae1ed4146df402a3bc2d6a2ee991cde9a1
Pingchuan Ma <pika7ma@gmail.com>
2021-06-26 17:29:23 -0400

    change credit

--- js/script.js
+++ js/script.js
@@ -7,19 +7,28 @@ moreContent.addEventListener("shown.bs.collapse", function() {
 
 var counter = 0;
 var hoverImages = [
-  "images/profile/McFlurry.png",
-  "images/profile/Sophie.png",
-  "images/profile/Sofia.png",
-  "images/profile/Mayo.png",
+  "images/profile/mcflurry.webp",
+  "images/profile/daimi.webp",
+  "images/profile/shomi.webp",
+  "images/profile/mayo.webp",
+];
+var hoverCredits = [
+  "Hello from McFlurry",
+  "Hello from Daimi",
+  "Hello from Shomi",
+  "Hello from Mayo",
 ];
 
 var profile = document.getElementById("profile");
+var credit = document.getElementById("credit");
 profile.addEventListener("mouseenter", function() {
   this.src = hoverImages[counter];
+  credit.innerText = hoverCredits[counter];
   counter = (counter + 1) % hoverImages.length;
 });
 profile.addEventListener("mouseleave", function() {
   this.src = "images/profile/myself.webp";
+  credit.innerText = "Photo by @QC";
 });
 
 var d = new Date();
