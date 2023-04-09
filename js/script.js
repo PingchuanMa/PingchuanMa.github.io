1c2a709fe1807df6d287df2d78e388a7f4d3be00
Pingchuan Ma <pika7ma@gmail.com>
2023-04-09 17:49:14 -0400

    update photo

--- js/script.js
+++ js/script.js
@@ -12,26 +12,26 @@ var hoverImages = [
   "images/profile/shomi.webp",
   "images/profile/mcflurry.webp",
 ];
-var hoverCredits = [
-  "Hello from <a title='CUDA' href='https://www.instagram.com/cudability/' target='_blank' rel='noopener noreferrer'>@CUDA</a>",
-  "Hello from <a title='Daimi' href='https://www.instagram.com/ragdolls_sophie_sofia/' target='_blank' rel='noopener noreferrer'>@Daimi</a>",
-  "Hello from <a title='Shomi' href='https://www.instagram.com/ragdolls_sophie_sofia/' target='_blank' rel='noopener noreferrer'>@Shomi</a>",
-  "Hello from McFlurry",
-];
+// var hoverCredits = [
+//   "Hello from <a title='CUDA' href='https://www.instagram.com/cudability/' target='_blank' rel='noopener noreferrer'>@CUDA</a>",
+//   "Hello from <a title='Daimi' href='https://www.instagram.com/ragdolls_sophie_sofia/' target='_blank' rel='noopener noreferrer'>@Daimi</a>",
+//   "Hello from <a title='Shomi' href='https://www.instagram.com/ragdolls_sophie_sofia/' target='_blank' rel='noopener noreferrer'>@Shomi</a>",
+//   "Hello from McFlurry",
+// ];
 
 var profile_credit = document.getElementById("profile-credit");
 var profile = document.getElementById("profile");
-var credit = document.getElementById("credit");
+// var credit = document.getElementById("credit");
 
 function profile_enter() {
   profile.src = hoverImages[counter];
-  credit.innerHTML = hoverCredits[counter];
+  // credit.innerHTML = hoverCredits[counter];
   counter = (counter + 1) % hoverImages.length;
 }
 
 function profile_leave() {
   profile.src = "images/profile/myself.webp";
-  credit.innerText = "Photo by Jiayuan Mao";
+  // credit.innerText = "Photo by Lijie Chen";
 }
 
 profile_credit.addEventListener("mouseenter", profile_enter);
