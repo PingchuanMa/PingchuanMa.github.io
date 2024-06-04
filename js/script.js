a68a31308103cebe60725537a7c6796f99a3b13b
Pingchuan Ma <pika7ma@gmail.com>
2024-06-03 23:12:59 -0700

    update

--- js/script.js
+++ js/script.js
@@ -2,8 +2,6 @@
 
 var moreContent = document.getElementById("more-content");
 moreContent.addEventListener("shown.bs.collapse", function() {
-  // let trigger = document.getElementById("more-trigger");
-  // trigger.style.visibility = "hidden";
   this.scrollIntoView();
 });
 
@@ -41,7 +39,21 @@ document.getElementById('year').innerHTML = d.getFullYear();
 var snippets = document.querySelectorAll('.snippet');
 
 [].forEach.call(snippets, function(snippet) {
-  snippet.firstChild.insertAdjacentHTML('beforebegin', '<button class="btn" data-clipboard-snippet><i class="bi bi-copy"></i></button>');
+  snippet.firstChild.insertAdjacentHTML(
+    'beforebegin',
+    '<button class="btn" data-bs-toggle="tooltip" data-bs-title="Copy to clipboard" data-clipboard-snippet><i class="bi bi-clipboard"></i></button>');
+});
+
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+tooltipTriggerList.forEach(button => {
+  button.addEventListener('mouseleave', function() {
+    this.querySelector('i').className = 'bi bi-clipboard';
+    var tooltip = bootstrap.Tooltip.getInstance(this);
+    tooltip.setContent({'.tooltip-inner': 'Copy to clipboard'});
+    tooltip.hide();
+  });
 });
 
 var clipboardSnippets = new ClipboardJS('[data-clipboard-snippet]', {
@@ -52,4 +64,7 @@ var clipboardSnippets = new ClipboardJS('[data-clipboard-snippet]', {
 
 clipboardSnippets.on('success', function(e) {
   e.clearSelection();
+  e.trigger.querySelector('i').className = 'bi bi-clipboard-check';
+  var tooltip = bootstrap.Tooltip.getInstance(e.trigger);
+  tooltip.setContent({'.tooltip-inner': 'Copied!'});
 });
